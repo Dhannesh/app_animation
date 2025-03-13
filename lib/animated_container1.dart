3f2a267b1771e92b56043ad15ae31661efb0bc7f
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-03-13 09:39:14 +0530

    AnimateContainer changes screen size and color

--- lib/animated_container1.dart
+++ lib/animated_container1.dart
@@ -9,6 +9,8 @@ class MyAnimatedContainer1 extends StatefulWidget {
 
 class _AnimatedContainer1State extends State<MyAnimatedContainer1> {
   Color _color = Colors.redAccent;
+  double _width = 50.0;
+  double _height = 50.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +22,16 @@ class _AnimatedContainer1State extends State<MyAnimatedContainer1> {
         body: Center(
           child: AnimatedContainer(
             color: _color,
+            width: _width,
+            height: _height,
             duration: Duration(milliseconds: 2500),
           ),
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {
           setState(() {
             _color = (_color == Colors.redAccent )? Colors.greenAccent : Colors.redAccent;
+            _width = (_width == 50.0) ? MediaQuery.of(context).size.width : 50.0;
+            _height = (_height == 50.0) ? MediaQuery.of(context).size.height : 50.0;
           });
         },
         child: const Icon(Icons.play_arrow),
