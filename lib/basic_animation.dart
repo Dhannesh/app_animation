6e57800bf029894e0281d201bef5a52be9a6e218
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-03-13 09:08:11 +0530

    add AnimateContainer to animate

--- lib/basic_animation.dart
+++ lib/basic_animation.dart
@@ -12,23 +12,19 @@ class BasicAnimation extends StatefulWidget {
 
 class _BasicAnimationState extends State<BasicAnimation> {
   double _side = 50.0;
-  Color _color = Color.fromARGB(
-      Random.secure().nextInt(255),
-      Random.secure().nextInt(255),
-      Random.secure().nextInt(255),
-      Random.secure().nextInt(255));
-  Timer? timer;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Expanded(
             child: Center(
-          child: Container(
+          child: AnimatedContainer(
             width: _side,
             height: _side,
             alignment: Alignment.center,
-            decoration: BoxDecoration(color: _color),
+            decoration: BoxDecoration(color: Colors.cyan),
+            duration: const Duration(milliseconds: 1000),
           ),
         )),
         Padding(
@@ -39,32 +35,8 @@ class _BasicAnimationState extends State<BasicAnimation> {
                   child: ElevatedButton(
                       child: Text('Shrink'),
                       onPressed: () {
-                        timer?.cancel();
-                        timer = Timer.periodic(Duration(milliseconds: 100),
-                            (timer) {
-                          setState(() {
-                            if (_side > 1) {
-                              _side -= 2;
-                              if (_side % 10 == 0) {
-                                _color = Color.fromARGB(
-                                    Random.secure().nextInt(255),
-                                    Random.secure().nextInt(255),
-                                    Random.secure().nextInt(255),
-                                    Random.secure().nextInt(255));
-                              }
-                            }
-                          });
-                        });
-                      })),
-              const SizedBox(
-                width: 8,
-              ),
-              Expanded(
-                  child: ElevatedButton(
-                      child: Text('Stop'),
-                      onPressed: () {
                         setState(() {
-                          timer?.cancel();
+                          _side = 20;
                         });
                       })),
               const SizedBox(
@@ -74,21 +46,8 @@ class _BasicAnimationState extends State<BasicAnimation> {
                   child: ElevatedButton(
                       child: Text('Grow'),
                       onPressed: () {
-                        timer?.cancel();
-                        timer = Timer.periodic(Duration(milliseconds: 100),
-                            (timer) {
-                          setState(() {
-                            if (_side < 300) {
-                              _side += 2;
-                              if (_side % 10 == 0) {
-                                _color = Color.fromARGB(
-                                    Random.secure().nextInt(255),
-                                    Random.secure().nextInt(255),
-                                    Random.secure().nextInt(255),
-                                    Random.secure().nextInt(255));
-                              }
-                            }
-                          });
+                        setState(() {
+                          _side = 300;
                         });
                       })),
             ],
